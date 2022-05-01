863254465219f0731c3cb45fa327ce75574e7f62
LeeTrent <lee.trent.1@gmail.com>
2022-05-01 12:25:07 -0500

    Working on dockerizing authentication service

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -6,9 +6,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const webPort = "80"
+
+var counts int64 = 0
 
 type Config struct {
 	DB     *sql.DB
@@ -18,10 +26,17 @@ type Config struct {
 func main() {
 	log.Println("Starting authentication service...")
 
-	// TODO: Connect to DB
+	conn := connectToDB()
+	if conn != nil {
+		logSnippet := "[authentication-service][main.go] =>"
+		log.Panic(logSnippet + "Can't connect to PostgreSQL DB")
+	}
 
 	// Set up configuration...
-	app := Config{}
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -31,6 +46,48 @@ func main() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	logSnippet := "[authentication-service][main.go][connectToDB()] =>"
+
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+
+		if err != nil {
+			log.Printf("%s openDB() returned an error (incrementing count):\n", logSnippet)
+			log.Println(err)
+			counts++
+		} else {
+			log.Printf("%s openDB() did not return an error:\n", logSnippet)
+			return connection
+		}
+
+		if counts > 10 {
+			log.Printf("%s Error count has exceeded 10, could not connec to to database\n", logSnippet)
+			return nil
+		}
+
+		log.Printf("%s Sleeping for 2 seconds...\n", logSnippet)
+		time.Sleep(2 * time.Second)
+		continue
 	}
 
 }
