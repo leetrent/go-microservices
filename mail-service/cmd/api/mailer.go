d81957460524be2cdad69acb03820295750570a3
LeeTrent <lee.trent.1@gmail.com>
2022-05-06 12:53:05 -0500

    Completed developer of mail-service

--- mail-service/cmd/api/mailer.go
+++ mail-service/cmd/api/mailer.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bytes"
+	"html/template"
+	"log"
+	"time"
+
+	"github.com/vanng822/go-premailer/premailer"
+	mail "github.com/xhit/go-simple-mail/v2"
+)
+
 type Mail struct {
 	Domain      string
 	Host        string
@@ -22,6 +32,8 @@ type Message struct {
 }
 
 func (m *Mail) SendSMTPMessage(msg Message) error {
+	logSnippet := "\n[mail-service][mailer][SendSMTPMessage] =>"
+
 	if msg.From == "" {
 		msg.From = m.FromAddress
 	}
@@ -36,8 +48,132 @@ func (m *Mail) SendSMTPMessage(msg Message) error {
 	msg.DataMap = data
 
 	formattedMessage, err := m.buildHTMLMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	plainMessage, err := m.buildPlainTextMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	server := mail.NewSMTPClient()
+	server.Host = m.Host
+	server.Port = m.Port
+	server.Username = m.Username
+	server.Password = m.Password
+	server.Encryption = m.getEncryption(m.Encryption)
+	server.KeepAlive = false
+	server.ConnectTimeout = 10 * time.Second
+	server.SendTimeout = 10 * time.Second
+
+	smtpClient, err := server.Connect()
+	if err != nil {
+		log.Printf("%s (ERROR-server.Connect): %s", logSnippet, err.Error())
+		return err
+	}
+
+	email := mail.NewMSG()
+	email.SetFrom(msg.From).AddTo(msg.To).SetSubject(msg.Subject)
+	email.SetBody(mail.TextPlain, plainMessage)
+	email.AddAlternative(mail.TextHTML, formattedMessage)
+	if len(msg.Attachements) > 0 {
+		for _, x := range msg.Attachements {
+			email.AddAttachment(x)
+		}
+	}
+
+	err = email.Send(smtpClient)
+	if err != nil {
+		log.Printf("%s (ERROR- email.Send): %s", logSnippet, err.Error())
+		return err
+	}
+
+	return nil
 }
 
 func (m *Mail) buildHTMLMessage(msg Message) (string, error) {
+	logSnippet := "\n[mail-service][mailer][buildHTMLMessage] =>"
 
+	templateToRender := "./templates/mail.html.gohtml"
+
+	t, err := template.New("email-html").ParseFiles(templateToRender)
+	if err != nil {
+		log.Printf("%s (ERROR-template.New.ParseFiles): %s", logSnippet, err.Error())
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		log.Printf("%s (ERROR-t.ExecuteTemplate): %s", logSnippet, err.Error())
+		return "", err
+	}
+
+	formattedMessage := tpl.String()
+	formattedMessage, err = m.inlineCSS(formattedMessage)
+	if err != nil {
+		log.Printf("%s (ERROR-m.inlineCSS): %s", logSnippet, err.Error())
+		return "", err
+	}
+
+	return formattedMessage, nil
+}
+
+func (m *Mail) inlineCSS(s string) (string, error) {
+	logSnippet := "\n[mail-service][mailer][inlineCSS] =>"
+
+	options := premailer.Options{
+		RemoveClasses:     false,
+		CssToAttributes:   false,
+		KeepBangImportant: true,
+	}
+
+	prem, err := premailer.NewPremailerFromString(s, &options)
+	if err != nil {
+		log.Printf("%s (ERROR-premailer.NewPremailerFromString): %s", logSnippet, err.Error())
+		return "", err
+	}
+
+	html, err := prem.Transform()
+	if err != nil {
+		log.Printf("%s (ERROR-prem.Transform): %s", logSnippet, err.Error())
+		return "", err
+	}
+
+	return html, nil
+}
+
+func (m *Mail) buildPlainTextMessage(msg Message) (string, error) {
+	logSnippet := "\n[mail-service][mailer][buildPlainTextMessage] =>"
+
+	templateToRender := "./templates/mail.plain.gohtml"
+
+	t, err := template.New("email-plain").ParseFiles(templateToRender)
+	if err != nil {
+		log.Printf("%s (ERROR-template.New.ParseFiles): %s", logSnippet, err.Error())
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		log.Printf("%s (ERROR-t.ExecuteTemplate): %s", logSnippet, err.Error())
+		return "", err
+	}
+
+	plainMessage := tpl.String()
+
+	return plainMessage, nil
+}
+
+func (m *Mail) getEncryption(s string) mail.Encryption {
+	switch s {
+	case "tls":
+		return mail.EncryptionSTARTTLS
+	case "ssl":
+		return mail.EncryptionSSLTLS
+	case "none", "":
+		return mail.EncryptionNone
+	default:
+		return mail.EncryptionSTARTTLS
+	}
 }
--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,13 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 const webPort = "80"
 
 func main() {
 	logSnippet := "\n[mail-service][main][main] =>"
-	app := Config{}
+
+	mailer, err := createMail()
+	if err != nil {
+		log.Printf("%s (ERROR||createMail): %s", logSnippet, err.Error())
+		log.Panic(err)
+	}
+
+	app := Config{
+		Mailer: mailer,
+	}
 
 	log.Printf("%s (Starting mail service on port %s)", logSnippet, webPort)
 
@@ -19,9 +30,32 @@ func main() {
 		Handler: app.routes(),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Printf("%s (ERROR||srv.ListenAndServe): %s", logSnippet, err.Error())
 		log.Panic(err)
 	}
+}
+
+func createMail() (Mail, error) {
+	logSnippet := "\n[mail-service][main][createMail] =>"
+
+	port, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	if err != nil {
+		log.Printf("%s (ERROR-strconv.Atoi(MAIL_PORT): %s", logSnippet, err.Error())
+		return Mail{}, err
+	}
+
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("MAIL_FROM_NAME"),
+		FromAddress: os.Getenv("MAIL_FROM_ADDRESS"),
+	}
+
+	return m, nil
 }
